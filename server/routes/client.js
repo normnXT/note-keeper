093a22ae00401557984ced3c0e842c12c5bbeb42
normnXT <119769208+normnXT@users.noreply.github.com>
2024-03-24 16:30:33 -0400

    Added body-parser dependency for URL-encoding and JSON parsing

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const notesModel = require('./../models/notes');
 
 const router = express.Router();
+
+// middle-ware for parsing JSON and URL-encoded objects from user input before handling
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
 
 router.get('/', async (req, res) => {
     res.send('Hello World!');
