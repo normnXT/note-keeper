15e4356153eda5cfdc84ea69c21c1b3d91f6ca75
normnXT <119769208+normnXT@users.noreply.github.com>
2024-03-25 21:06:58 -0400

    organization and variable names in notes.js/server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-// const bodyParse = require('body-parser');
 const app = require('express')();
 
-// DB connection
-const db = require('./config/keys').mongoProdURI;
-mongoose.connect(db)
+const router = require('./routes/notes');
+const uri = require('./config/keys').mongoProdURI;
+
+// Route handling to follow /notes subdirectory
+app.use('/notes', router);
+
+// Database connection
+mongoose.connect(uri)
     .then(() => console.log(`Mongodb Connected`))
     .catch(error => console.log(error));
-
-const ClientRouter = require('./routes/notes');
-app.use('/notes', ClientRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
