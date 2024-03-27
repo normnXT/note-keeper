4685bd4dff1585c02fe6a85749f0db93578c1905
normnXT <119769208+normnXT@users.noreply.github.com>
2024-03-27 19:30:23 -0400

    added material-tailwind and heroicons dependencies, reformatted some config files with prettier

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const withMT = require("@material-tailwind/react/utils/withMT");
 
+module.exports = withMT({
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+});
