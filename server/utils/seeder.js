9c34c879449d03f117e5e436ee7729433a381d49
normnXT <119769208+normnXT@users.noreply.github.com>
2024-07-25 23:26:34 -0400

    reworked seeder to use id parameter in URL

--- server/utils/seeder.js
+++ server/utils/seeder.js
@@ -1,19 +1,21 @@
-const express = require("express");
+const express = require('express');
 const Note = require("../models/Note");
 const syntheticNotes = require("../data/syntheticNotes");
 
 const router = express.Router();
 router.use(express.json());
 
-router.post("/", async (req, res) => {
+router.post("/:id", async (req, res) => {
     try {
-        // Adding user ID to each note (assuming req.user.id exists and holds the user ID)
+        const userId = req.params.id;
+
+        // Add user ID to each note
         const notesWithUser = syntheticNotes.map(note => ({
-            user: "66916d64782702fce2b4df55",
+            user: userId,
             ...note,
         }));
 
-        // Insert all notes at once
+        // Insert all synthetic notes
         const insertedNotes = await Note.insertMany(notesWithUser);
         res.status(201).json(insertedNotes);
     } catch (err) {
